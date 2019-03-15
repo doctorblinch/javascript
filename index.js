d1dbf0377f48ff8cc2888789654bdad23571dba6
Ivan <docblinch@gmail.com>
2019-03-15 10:51:48 +0200

    Proxy http node js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 "use strict";
 
-console.log("Hello world");
+
+const http = require('http');
+
+const server = http.createServer((request, response) =>{
+  console.log(request.protocol);
+  console.log("URLS:", request.url);
+
+  response.setHeader('Testing','Tfd ds');
+
+  if (request.url.split('://')[0] === 'http'){
+    console.log('http, at URL', request.url);
+    http.get(request.url, (resp) =>{
+      resp.pipe(response);
+
+
+    }).on('error', err =>{
+      console.log('Error message: ', err.message);
+    });
+  }
+  else {
+    console.log('https');
+  }
+});
+
+server.listen(8080);
