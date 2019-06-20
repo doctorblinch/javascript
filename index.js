6cfdb4cad23ab2b7e5e5f274eca45a7e31304ab8
doctorblinch <docblinch@gmail.com>
2019-06-20 22:50:14 +0300

    seperate module

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const SECRET_KEY = '868853661:AAH36Ot5-90yT_QJTs2FOoLKLcQSyWThATk';
 const bot = new Telegraf(SECRET_KEY);
 bot.start((ctx) => ctx.reply('Hello, this is KPI rozklad bot!\nPrint group name whose schedual you want to see.'));
 bot.help((ctx) => ctx.reply('Print group name whose schedual you want to see.'));
-bot.hears(/^[А-ЯІа-яі]{2}-[1-9а-яі]{2,5}$/, (ctx) => parser(ctx.message.text)
+bot.hears(/^[А-ЯІа-яі]{2}-[1-9а-яі]{2,5}$/, (ctx) => parse(ctx.message.text)
     .then(result=>{ctx.reply(result)})
     .catch(()=>ctx.reply('Something went wrong.\nAre you sure that you choosen the right group and rozklad service is available?')));
 
