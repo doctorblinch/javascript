b36ca4b50dedacc45ba471780266b59ec0098f96
doctorblinch <docblinch@gmail.com>
2019-06-21 10:40:37 +0300

    nosov prievt 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const { parse } = require('./parser.js');
 const Telegraf = require('telegraf');
-const SECRET_KEY = process.env.BOT_TOKEN;//'868853661:AAH36Ot5-90yT_QJTs2FOoLKLcQSyWThATk';
+const SECRET_KEY = '868853661:AAH36Ot5-90yT_QJTs2FOoLKLcQSyWThATk';//process.env.BOT_TOKEN;//
 const bot = new Telegraf(SECRET_KEY);
 bot.start((ctx) => ctx.reply('Hello, this is KPI rozklad bot!\nPrint group name whose schedual you want to see.'));
 bot.help((ctx) => ctx.reply('Print group name whose schedual you want to see.'));
@@ -11,5 +11,5 @@ bot.hears(/^[А-ЯІа-яі]{2}-[1-9а-яі]{2,5}$/, (ctx) => parse(ctx.message.
     .catch(()=>ctx.reply('Something went wrong.\nAre you sure that you choosen the right group and rozklad service is available?')));
 
 //bot.launch()
-bot.telegram.setWebhook('https://nodelabs-kpi-schedule-bot.doctorblinch.now.sh');
+bot.telegram.setWebhook(process.env.WEB_HOOK_PATH);//'https://nodelabs-kpi-schedule-bot.doctorblinch.now.sh');
 module.exports = bot.webhookCallback('/');
